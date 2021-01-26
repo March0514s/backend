e8ac99d4846f36f7a5302fc22b2781197b829c3d
Marcus Davanço <md.daves@gmail.com>
2021-01-25 21:49:49 -0300

    feature: implemented cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { v4: uuid, validate } = require('uuid');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
